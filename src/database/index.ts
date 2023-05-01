740f25f8921a3c01e949868f80a2add0362be735
Tania Pedraza <taniapedraza97@gmail.com>
2023-05-01 10:09:09 -0500

    Remove seed

--- src/database/index.ts
+++ src/database/index.ts
@@ -2,6 +2,3 @@ export * as db from './db'
 export * as dbProducts from './dbProducts'
 export * as dbUsers from './dbUsers'
 export * from './constants'
-
-// Base de datos de prueba
-export * as seedDatabse from './products'
